fb07fc4c17761eee5873c4f3a324568486f73186
Robert_Jean <stevejean285@gmail.com>
2024-11-04 01:06:42 -0500

    Completed

--- src/index.js
+++ src/index.js
@@ -25,12 +25,34 @@ import { central, db1, db2, db3, vault } from "./databases.js";
     }
 }
  */
-function getUserData(id) {
+async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   };
-
-  const returnedValue = central(id); 
+  //obj to construct the result
+  let result = {}
+  try {
+    const databasetoquery = await central(id);
+    // get the funtion to call from the dictionary database based on tthe id
+    const queryDatabase = dbs[databasetoquery];
+    const results = await Promise.all([queryDatabase(id), vault(id)]);
+    // objects returned by db1--db3 have nested objects -- >structuredClone to make a deep copy
+    let result = structuredClone(results[0]);
+    //vault returns a simple object --> csn use spread to clone it 
+    result = { ...results[1] }
+    //adding the id key: value
+    result['id'] = id;
+    console.log(result);
+  }
+  catch {
+    console.log('Error Fetching user');
+  }
+  return new Promise ((resolve, reject)=>{
+    resolve(result);
+    reject(Error);
+  })
 }
+
+getUserData(3);
